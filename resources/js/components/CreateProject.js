1c65a31118b611abee8ecba3ff6ca9403c91e11e
Djordje Arsenovic <dj.arsenovic@gmail.com>
2020-10-03 14:41:48 +0200

    Refactored CreateProject component from class to functional component.

--- resources/js/components/CreateProject.js
+++ resources/js/components/CreateProject.js
@@ -1,115 +1,94 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import axios from "axios";
 
-class CreateProject extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      description: '',
-      errors: []
-    }
-    this.handleFieldChange = this.handleFieldChange.bind(this);
-    this.createNewProject = this.createNewProject.bind(this);
-    this.hasErrorFor = this.hasErrorFor.bind(this);
-    this.renderErrorFor = this.renderErrorFor.bind(this);
-  }
-
-  render() {
-    return (
-        <div className="container py-4">
-          <div className="row justify-content-center">
-            <div className="col-md-6">
-              <div className="card">
-                <div className="card-header">
-                  Create new project
-                </div>
-                <div className="card-body">
-                  <form onSubmit={this.createNewProject}>
-                    <div className="form-group">
-                      <label htmlFor="name">Project</label>
-                      <input type="text"
-                             id="name"
-                             name="name"
-                             autoComplete="off"
-                             value={this.state.name}
-                             onChange={this.handleFieldChange}
-                             className={`form-control ${this.hasErrorFor('name') ? 'is-invalid' : ''}`}/>
-                      {this.renderErrorFor('name')}
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="description">Project description</label>
-                      <textarea
-                          id="description"
-                          className={`form-control ${this.hasErrorFor('description') ? 'is-invalid' : ''}`}
-                          name="description"
-                          rows="10"
-                          value={this.state.description}
-                          onChange={this.handleFieldChange}
-                      />
-                      {this.renderErrorFor('description')}
-                    </div>
-                    <button className="btn btn-primary">Create</button>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    );
-  }
+const CreateProject = (props) => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [errors, setErrors] = useState([]);
 
   /**
+   * Creates a project
    * @param event
    */
-  createNewProject(event) {
+  const createNewProject = event => {
     event.preventDefault();
-    const {history} = this.props;
-    const project = {
-      name: this.state.name,
-      description: this.state.description
-    }
-    axios.post('/api/projects', project).then(response => {
+    const {history} = props;
+    axios.post('/api/projects', {
+      name,
+      description
+    }).then(response => {
       history.push('/');
     }).catch(error => {
-      this.setState({
-        errors: error.response.data.errors
-      });
+      setErrors(error.response.data.errors);
     });
-  }
+  };
 
   /**
+   * Checks for errors
    * @param field
    * @returns {boolean}
    */
-  hasErrorFor(field) {
-    return !!this.state.errors[field];
+  const hasErrorFor = field => {
+    return !!errors[field];
   }
 
   /**
-   * @param event
-   */
-  handleFieldChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
-
-  /**
-   *
+   * Renders the error on the field.
    * @param field
    * @returns {JSX.Element}
    */
-  renderErrorFor(field) {
-    if (this.hasErrorFor(field)) {
+  const renderErrorFor = field => {
+    if (hasErrorFor(field)) {
       return (
           <span className="invalid-feedback">
-            <strong>{this.state.errors[field][0]}</strong>
+            <strong>{errors[field][0]}</strong>
           </span>
       );
     }
   }
+
+  return (
+      <div className="container py-4">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-header">
+                Create new project
+              </div>
+              <div className="card-body">
+                <form onSubmit={createNewProject}>
+                  <div className="form-group">
+                    <label htmlFor="name">Project</label>
+                    <input type="text"
+                           id="name"
+                           name="name"
+                           autoComplete="off"
+                           value={name}
+                           onChange={event => setName(event.target.value)}
+                           className={`form-control ${hasErrorFor('name') ? 'is-invalid' : ''}`}/>
+                    {renderErrorFor('name')}
+                  </div>
+
+                  <div className="form-group">
+                    <label htmlFor="description">Project description</label>
+                    <textarea
+                        id="description"
+                        className={`form-control ${hasErrorFor('description') ? 'is-invalid' : ''}`}
+                        name="description"
+                        rows="10"
+                        value={description}
+                        onChange={event => setDescription(event.target.value)}
+                    />
+                    {renderErrorFor('description')}
+                  </div>
+                  <button className="btn btn-primary">Create</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
 }
 
 export default CreateProject;
