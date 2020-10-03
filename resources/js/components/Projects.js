beafae1496ec8685008b94d005a1790fb04ea33e
Djordje Arsenovic <dj.arsenovic@gmail.com>
2020-10-03 15:12:51 +0200

    Refactored Projects component from class to functional component.

--- resources/js/components/Projects.js
+++ resources/js/components/Projects.js
@@ -1,54 +1,42 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {Link} from "react-router-dom";
 
-class Projects extends Component {
-  constructor() {
-    super();
-    this.state = {
-      projects: []
-    }
-  }
-
-  componentDidMount() {
+const Projects = () => {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
     axios.get('/api/projects').then(response => {
-      this.setState({
-        projects: response.data
-      });
+      setProjects(response.data);
     });
-  }
-
-  render() {
-    const {projects} = this.state;
-    return (
-        <div className="container py-4">
-          <div className="row justify-content-center">
-            <div className="col-md-8">
-              <div className="card">
-                <div className="card-header">All projects</div>
-                <div className="card-body">
-                  <Link to='/create' className="btn btn-primary btn-sm mb-3">
-                    Create new project
-                  </Link>
-                  <ul className="list-group list-group-flush">
-                    {projects.map(project => (
-                        <Link to={`/${project.id}`}
-                              key={project.id}
-                              className="d-flex flex-row justify-content-between align-content-center align-items-center">
-                          {project.name}
-                          <span className="badge badge-primary badge-pill">
+  }, []);
+  return (
+      <div className="container py-4">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <div className="card">
+              <div className="card-header">All projects</div>
+              <div className="card-body">
+                <Link to='/create' className="btn btn-primary btn-sm mb-3">
+                  Create new project
+                </Link>
+                <ul className="list-group list-group-flush">
+                  {projects.map(project => (
+                      <Link to={`/${project.id}`}
+                            key={project.id}
+                            className="d-flex flex-row justify-content-between align-content-center align-items-center">
+                        {project.name}
+                        <span className="badge badge-primary badge-pill">
                             {project.tasks_count}
                           </span>
-                        </Link>
-                    ))}
-                  </ul>
-                </div>
+                      </Link>
+                  ))}
+                </ul>
               </div>
             </div>
           </div>
         </div>
-    );
-  }
+      </div>
+  );
 }
 
 export default Projects;
